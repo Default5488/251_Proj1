5e57c92997e268f7acedc209156bc67fc62d1af6
Tyler Kooper <takbek08@live.com>
2022-09-15 17:34:20 +0000

    Milestones 1 & 2 completed:
    Revised the program:
    -No ptrs
    -Only ourvectors
    
    Moving onto milestones 3-6
    
    Date: 9/15/2022 : Good Job Buddy! :)

--- .ccls-cache/@home@runner@project1/ourvector.h
+++ .ccls-cache/@home@runner@project1/ourvector.h
@@ -2,7 +2,7 @@
 
 //
 // ourvector class --- a replacement for std::vector using the same
-// dynamic array approach.  Starts with initial capacity of 4, and 
+// dynamic array approach.  Starts with initial capacity of 4, and
 // doubles in size as needed.  Usage statistics are also collected
 // and output to stderr when the last vector is destroyed.
 //
@@ -12,88 +12,79 @@
 
 #pragma once
 
-#include <iostream>
 #include <exception>
+#include <iostream>
 #include <stdexcept>
 #include <typeinfo>
 
 using namespace std;
 
-
 //
 // ourvector<T>
 //
-template<typename T>
-class ourvector
-{
+template <typename T> class ourvector {
 private:
   //
-  // A vector is a dynamically-allocated array of elements, of type T.  
-  // Since it's a low-level C-style array, we need to keep track of how 
+  // A vector is a dynamically-allocated array of elements, of type T.
+  // Since it's a low-level C-style array, we need to keep track of how
   // many elements we have stored in the array (Size), and the total # of
   // available locations (Capacity).  When it's full, we double in size.
   //
-  T*  A;         // pointer to dynamically-allocated C-style array    
-  int Size;      // # of elements in the array
-  int Capacity;  // total # of locations available
+  T *A;         // pointer to dynamically-allocated C-style array
+  int Size;     // # of elements in the array
+  int Capacity; // total # of locations available
 
   //
   // Global stats on vector usage:
   //
-  static int InUse;     // # of vector objects currently in use
-  static int Vectors;   // # of vector objects created
-  static int Inserts;   // # of elements inserted into the vector
-  static int Accesses;  // # of times elements were accessed (read or written)
+  static int InUse;    // # of vector objects currently in use
+  static int Vectors;  // # of vector objects created
+  static int Inserts;  // # of elements inserted into the vector
+  static int Accesses; // # of times elements were accessed (read or written)
 
   //
   // Iterators are objects that allow iteration in a structure-neutral way.
   // For this vector class, we keep track of the vector object and the current
   // index into the vector.  As we iterate, we advance the index.
   //
-  class iterator
-  {
+  class iterator {
   private:
-    ourvector* V;         // which vector are we iterating through?
-    int        CurIndex;  // which element are we currently on?
+    ourvector *V; // which vector are we iterating through?
+    int CurIndex; // which element are we currently on?
 
   public:
     // default constructor: denotes first element
-    iterator(ourvector* underlyingVector)
-    {
+    iterator(ourvector *underlyingVector) {
       V = underlyingVector;
       CurIndex = 0;
     }
 
     // constructor: denotes ith element
-    iterator(ourvector* underlyingVector, int i)
-    {
+    iterator(ourvector *underlyingVector, int i) {
       V = underlyingVector;
       CurIndex = i;
     }
 
     // advance:
-    iterator& operator++()
-    {
+    iterator &operator++() {
       CurIndex++;
 
       return *this;
     }
 
     // access current element:
-    T& operator*()
-    {
+    T &operator*() {
       V->Accesses++;
-      
+
       return V->A[CurIndex];
     }
 
     // compare iterators, e.g. to stop iterating:
-    bool operator!=(const iterator& rhs)
-    {
+    bool operator!=(const iterator &rhs) {
       // is this iterator != to rhs iterator?
-      if (V != rhs.V)  // different vectors?
+      if (V != rhs.V) // different vectors?
         return true;
-      if (CurIndex != rhs.CurIndex)  // different index?
+      if (CurIndex != rhs.CurIndex) // different index?
         return true;
 
       // same underlying vector and index, so iterators denote same element:
@@ -101,17 +92,15 @@ private:
     }
   };
 
-
 public:
   //
   // default constructor:
   //
-  // Called automatically by C++ to create an empty vector with an 
+  // Called automatically by C++ to create an empty vector with an
   // initial capacity of 4.
   //
-  ourvector()
-  {
-    A = new T[4];  // we start with a capacity of 4:
+  ourvector() {
+    A = new T[4]; // we start with a capacity of 4:
     Size = 0;
     Capacity = 4;
 
@@ -126,8 +115,7 @@ public:
   // of an existing vector.  Example: this occurs when passing ourvector as
   // a parameter by value.
   //
-  ourvector(const ourvector& other)
-  {
+  ourvector(const ourvector &other) {
     //
     // allocate this vector with same capacity as the other:
     //
@@ -141,8 +129,7 @@ public:
     //
     // now make a copy the elements from the other vector into this one:
     //
-    for (int i = 0; i < other.Size; ++i)
-    {
+    for (int i = 0; i < other.Size; ++i) {
       this->A[i] = other.A[i];
 
       Accesses++;
@@ -162,10 +149,9 @@ public:
   // no longer exists.  Example: this is called when a function returns a
   // vector as a result ---so the result is moved instead of copied.
   //
-  ourvector(ourvector&& other)
-  {
+  ourvector(ourvector &&other) {
     //
-    // NOTE: we can "move" the elements over by simply pointing to the 
+    // NOTE: we can "move" the elements over by simply pointing to the
     // array in the other vector:
     //
     this->A = other.A;
@@ -194,16 +180,15 @@ public:
   //
   // Called automatically by C++ to free the memory associated by the vector.
   //
-  virtual ~ourvector()
-  {
-    if (A != nullptr)  // then we have something to free:
+  virtual ~ourvector() {
+    if (A != nullptr) // then we have something to free:
     {
-      delete[] A;  // since we new'd an array, we delete[] the same array
+      delete[] A; // since we new'd an array, we delete[] the same array
 
-      InUse--;  // one fewer vector in use:
+      InUse--; // one fewer vector in use:
     }
 
-    if (InUse == 0)  // last vector was just deleted, so output stats:
+    if (InUse == 0) // last vector was just deleted, so output stats:
     {
       _stats();
     }
@@ -215,15 +200,14 @@ public:
   // Empties the vector of all elements, reseting size to 0 and capacity
   // to 4.
   //
-  void clear()
-  {
+  void clear() {
     //
-    // to make sure elements are properly freed, let's free the array and 
+    // to make sure elements are properly freed, let's free the array and
     // reallocate:
     //
-    delete[] A;  // since we new'd an array, we delete[] an array
-    
-    A = new T[4];  // reset to a new, empty vector with capacity of 4:
+    delete[] A; // since we new'd an array, we delete[] an array
+
+    A = new T[4]; // reset to a new, empty vector with capacity of 4:
     Size = 0;
     Capacity = 4;
   }
@@ -233,19 +217,18 @@ public:
   //
   // Called when you assign one vector into another, i.e. this = other;
   //
-  ourvector& operator=(const ourvector& other)
-  {
+  ourvector &operator=(const ourvector &other) {
     //
     // special case: are we assigning to ourself?
     //
     if (this == &other)
       return *this;
 
-    // 
+    //
     // unlike a copy constructor, *this* vector exists and so we first
     // have to free the memory associated with this vector:
     //
-    delete[] A;  // since we new'd an array, we delete[] an array
+    delete[] A; // since we new'd an array, we delete[] an array
 
     //
     // allocate this array with same capacity as the other:
@@ -257,8 +240,7 @@ public:
     //
     // now make a copy the elements from the other vector into this one:
     //
-    for (int i = 0; i < other.Size; ++i)
-    {
+    for (int i = 0; i < other.Size; ++i) {
       this->A[i] = other.A[i];
 
       Accesses++;
@@ -268,28 +250,27 @@ public:
     //
     // done, this vector is now a copy of the other vector:
     //
-    return *this;  // we have to return a vector based on how = works:
+    return *this; // we have to return a vector based on how = works:
   }
 
   //
   // move operator=
   //
-  // Called when you move one vector into another, i.e. 
+  // Called when you move one vector into another, i.e.
   // this = std::move(other);
   //
-  ourvector& operator=(ourvector&& other)
-  {
+  ourvector &operator=(ourvector &&other) {
     //
     // special case: are we moving to ourself?
     //
     if (this == &other)
       return *this;
 
-    // 
+    //
     // unlike a move constructor, *this* vector exists and so we first
     // have to free the memory associated with this vector:
     //
-    delete[] A;  // since we new'd an array, we delete[] an array
+    delete[] A; // since we new'd an array, we delete[] an array
 
     //
     // now move the *other* data to this vector:
@@ -306,11 +287,11 @@ public:
     other.Capacity = 0;
 
     --InUse;
-      
+
     //
     // done, this vector now owns the data from the *other* vector:
     //
-    return *this;  // we have to return a vector based on how = works:
+    return *this; // we have to return a vector based on how = works:
   }
 
   //
@@ -318,41 +299,32 @@ public:
   //
   // Returns the # of elements currently in the array.
   //
-  int size() const
-  {
-    return Size;
-  }
+  int size() const { return Size; }
 
   //
   // capacity:
   //
-  // Returns the total # of available locations in the vector before it 
+  // Returns the total # of available locations in the vector before it
   // will need to be resized; resizing happens automatically when full.
   //
-  int capacity() const
-  {
-    return Capacity;
-  }
+  int capacity() const { return Capacity; }
 
   //
   // push_back:
   //
-  // Adds e to the end of the vector; if the vector is full, it's capacity 
+  // Adds e to the end of the vector; if the vector is full, it's capacity
   // is doubled and then e is added.
   //
-  void push_back(T e)
-  {
+  void push_back(T e) {
     //
     // is the vector full?  if so, we have to double in size before we can add:
     //
-    if (Size == Capacity)
-    {
+    if (Size == Capacity) {
       int newCapacity = Capacity * 2;
-      T* newA = new T[newCapacity];
+      T *newA = new T[newCapacity];
 
       // copy the elements from A to the new array:
-      for (int i = 0; i < Size; ++i)
-      {
+      for (int i = 0; i < Size; ++i) {
         newA[i] = A[i];
       }
 
@@ -375,14 +347,12 @@ public:
   //
   // at(i):
   //
-  // Returns a reference to the ith elements in the vector, which allows 
+  // Returns a reference to the ith elements in the vector, which allows
   // you to read (access) or write (modify) this element.  If i is out of
   // bounds, an exception is thrown.
   //
-  T& at(int i)
-  {
-    if (i < 0 || i >= Size)
-    {
+  T &at(int i) {
+    if (i < 0 || i >= Size) {
       throw out_of_range("ourvector: i out of bounds");
     }
 
@@ -394,29 +364,24 @@ public:
   //
   // [i]:
   //
-  // Returns a reference to the ith elements in the vector, which allows 
+  // Returns a reference to the ith elements in the vector, which allows
   // you to read (access) or write (modify) this element.  If i is out of
   // bounds, an exception is thrown.  This is very convenient, since it
   // allows you to treat a vector like a built-in array:
   //
   //   cout << V[i] << endl;
   //
-  T& operator[](int i)
-  {
-    return at(i);
-  }
+  T &operator[](int i) { return at(i); }
 
   //
   // front:
-  // 
-  // Returns a reference to the first elements in the vector, which allows 
-  // you to read (access) or write (modify) this element.  If the vector is
-  // empty, an exception is thrown. 
   //
-  T& front()
-  {
-    if (Size == 0)
-    {
+  // Returns a reference to the first elements in the vector, which allows
+  // you to read (access) or write (modify) this element.  If the vector is
+  // empty, an exception is thrown.
+  //
+  T &front() {
+    if (Size == 0) {
       throw runtime_error("ourvector: empty");
     }
 
@@ -427,21 +392,19 @@ public:
 
   //
   // back:
-  // 
-  // Returns a reference to the last elements in the vector, which allows 
+  //
+  // Returns a reference to the last elements in the vector, which allows
   // you to read (access) or write (modify) this element.  If the vector is
-  // empty, an exception is thrown. 
-  //  
-  T& back()
-  {
-    if (Size == 0)
-    {
+  // empty, an exception is thrown.
+  //
+  T &back() {
+    if (Size == 0) {
       throw runtime_error("ourvector: empty");
     }
 
     Accesses++;
 
-    return A[Size-1];
+    return A[Size - 1];
   }
 
   //
@@ -451,30 +414,23 @@ public:
   // in the vector.  Iterators are used with the built-in C++ algorithms
   // to iterate through various data structures in an abstract way.
   //
-  iterator begin()
-  {
-    return iterator(this);
-  }
+  iterator begin() { return iterator(this); }
 
   //
   // end:
   //
   // Returns an iterator (think pointer) denoting the end of the iteration
-  // space.  Iterators are used with the built-in C++ algorithms to 
+  // space.  Iterators are used with the built-in C++ algorithms to
   // iterate through various data structures in an abstract way.
   //
-  iterator end()
-  {
-    return iterator(this, Size);
-  }
+  iterator end() { return iterator(this, Size); }
 
   //
   // _output:
   //
   // Outputs the contents of the vector to stdout; for debugging purposes.
   //
-  void _output()
-  {
+  void _output() {
     cout << "** ourvector: ";
 
     for (int i = 0; i < Size; ++i)
@@ -488,16 +444,14 @@ public:
   //
   // Outputs vector usage stats to stderr; for debugging purposes.
   //
-  void _stats()
-  {
+  void _stats() {
     string name = typeid(T).name();
 
     //
     // if the name is complicated, strip off just the prefix:
     //
     size_t pos = name.find('<');
-    if (pos != string::npos)
-    {
+    if (pos != string::npos) {
       name = name.substr(0, pos);
     }
 
@@ -508,20 +462,15 @@ public:
     cerr << " # of elements accessed: " << Accesses << endl;
     cerr << "*********************************************************" << endl;
   }
-
 };
 
 //
 // For collecting stats on vector usage:
 //
-template<typename T>
-int ourvector<T>::InUse = 0;
+template <typename T> int ourvector<T>::InUse = 0;
 
-template<typename T>
-int ourvector<T>::Vectors = 0;
+template <typename T> int ourvector<T>::Vectors = 0;
 
-template<typename T>
-int ourvector<T>::Inserts = 0;
+template <typename T> int ourvector<T>::Inserts = 0;
 
-template<typename T>
-int ourvector<T>::Accesses = 0;
+template <typename T> int ourvector<T>::Accesses = 0;
--- main.cpp
+++ main.cpp
@@ -1,31 +1,172 @@
 //
 //  Project 1 Starter Code - DNA Profiling
 //  TODO:  Put your header info here.  Update line above.
-//  
+//
 //
 
 #include "ourvector.h"
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
-int main() {
-    //TODO:  Write your code here.  You should have LOTS of function decomposition.
+struct person {
+  string name;
+  ourvector<ourvector<char>>
+      nucleo; // Needs to be a row of vector sets, holding the pushed back chars
+  ourvector<int> nucleoC;
+};
 
-	string input;
-	
-	bool abort = false;
+/* Helper function: parse Sequence
+INFO: Takes in: array of Database, full getline readin, and position in the
+database array
 
-	while(!abort){
-		cin >> input;
-		
-		switch(input){
-			case "#":
-				cout << "Ending" << endl;//Remove at end, commenting purpose only
-				break;
-			case default:
-				cout << "Invalid input" << endl;
-				break;
-		}
-	}
-	return 0;
+
+Used to parse out the top line of the database files
+--Only used to filter the top line of the file--
+*/
+ourvector<ourvector<char>> parseSeq(string line) { // Returns parsed
+  stringstream ss(line);                           // Search for delim
+  string token;                                    // Push back values
+  ourvector<char> temp;           // temp char vec, to push into vec-o-vec
+  ourvector<ourvector<char>> tmp; //
+
+  while (getline(ss, token, ',')) {
+    if (token == "name")
+      continue;
+    else {
+      for (auto &ch : token) {
+        temp.push_back(ch);
+        cout << ch;
+      }
+      cout << endl;
+      tmp.push_back(temp);
+      temp.clear();
+    }
+  }
+  return tmp;
 }
+
+/** Helper function: parse Person
+INFO: Takes in: array of Database, full getline readin, and position in the
+database array
+
+Used to parse persons name & dna sequence count into struct
+**/
+ourvector<int> parsePerson(
+    string line) { // Person will increment each new line that is feed through
+  string temp = line;
+  string parse, strTmp;
+  ourvector<int> tmpC;
+
+  parse = temp.substr(0, temp.find(','));
+  temp = temp.substr(temp.find(',') + 1, temp.size() - 1);
+
+  istringstream ss(temp);
+  while (getline(ss, strTmp, ',')) {
+    tmpC.push_back(stoi(strTmp));
+  }
+  return tmpC;
+}
+
+string parseName(string line) { // Returns the name
+  return line.substr(0, line.find(','));
+}
+
+/**
+@define db_load
+
+readIn given text file
+parse information into person struct for each individual
+**/
+ourvector<person> db_load(
+    string filename) { // returns person object to be pushed into vector array
+  string content;
+  fstream readIn;
+  int count = 0;
+	// ourvector<ourvector<person> > personVec;
+	ourvector<person> personVec;
+	person person;
+	
+	readIn.open(filename);
+	
+  if (!readIn.is_open()) {    // Checks if file is open
+    cout << "Failed" << endl; //!!Remove eventually
+  }
+
+  while (!readIn.eof()) {     // Loops until file end
+    getline(readIn, content); // Grabs full line, to pass to be delim functions
+    if (!readIn.good()) { // Checks if readIn is good
+      cout << "Bad readIn" << endl;
+      continue;
+    }
+
+    if (count == 0) 										// Filters for first line
+      person.nucleo = parseSeq(content); // Returns a vec<vec<char>> to store into person
+    else {                   						// Filters for Name & integers
+      person.name = parseName(content); // Catches name return
+      person.nucleoC = parsePerson(content); // Filters for quantity of person type
+    }
+		personVec.push_back(person);
+    count++;
+  }
+  return personVec;
+}
+
+void display(ourvector<ourvector<person> > db) {
+  for (int i = 0; i < db[0][0].nucleo.size(); i++) {//prints out the nucleo types
+    for (int j = 0; j < db[0][0].nucleo[i].size(); j++) {
+      cout << db[0][0].nucleo[i][j];
+    }
+    cout << " ";
+  }
+  cout << endl;
+  for (int k = 0; k < 24; k++) {//Change 24 to the size of the internal array
+    cout << db[0][k].name << flush;
+    for (auto &nm : db[0][k].nucleoC) {
+      cout << ", " << nm;
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  ourvector<ourvector<person>> database;
+  string command, file;
+  bool abort = false;
+
+  cout << "Welcome to the DNA Profiling Application." << endl;
+  cout << "Enter command of # to exit:";
+  cin >> command >> file;
+  cout << endl << "Loading database..." << endl;
+
+  while (!abort) {
+    if (command == "load_db") {
+      database.push_back(db_load(file));
+    } 
+		else if (command == "a") { // Remove testing purpose only
+      database.push_back(db_load("large.txt"));
+    } 
+		else if (command == "#") {
+      break;
+    } 
+		else if (command == "d") {
+      display(database);
+    } else {
+      cout << "load_db Not called.." << endl;
+      abort = true;
+    }
+
+    cout << "Enter command or # to exit:";
+    cin >> command >> file;
+    cout << endl;
+  }
+  return 0;
+}
+
+// Ref:
+// Struct:
+// https://stackoverflow.com/questions/7671719/creating-multiple-objects-for-classes-structures/7671753
+//
+// clear && rm -f program.exe && g++ -g -std=c++11 -Wall main.cpp -o program.exe
+// && ./program.exe
